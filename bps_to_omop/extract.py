ab544a27cfbfb2cdcdfad23d581075b7989583fd
Isidoro Gutierrez <isidoro.gutierrez@juntadeandalucia.es>
2025-01-17 09:43:52 +0000

    added datasets script to retrieve data_dir

--- bps_to_omop/extract.py
+++ bps_to_omop/extract.py
@@ -13,7 +13,7 @@ import pyarrow as pa
 import pyarrow.compute as pc
 import yaml
 
-from src.datasets import data_dir
+from datasets import data_dir
 
 # TODO: rename initialize_extraction so it feels more like a lookup function, which it is. Maybe remove it altogether?
 
