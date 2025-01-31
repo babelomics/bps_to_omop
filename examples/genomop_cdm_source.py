e4abbb18fe5a884d59de19a5486a7bae73b64fef
Isidoro Gutierrez <isidoro.gutierrez@juntadeandalucia.es>
2025-01-31 10:24:11 +0000

    add docs and example for CDM_SOURCE

---
+++ examples/genomop_cdm_source.py
@@ -0,0 +1,42 @@
+# %%
+import os
+import sys
+
+import pyarrow as pa
+from package.datasets import data_dir
+from pyarrow import parquet
+
+sys.path.append("./external/bps_to_omop/")
+import bps_to_omop.extract as ext
+import bps_to_omop.general as gen
+from bps_to_omop.omop_schemas import omop_schemas
+
+# %%
+# == Parameters =======================================================
+params_file = "./package/preomop/genomop_cdm_source_params.yaml"
+
+# -- Load parameters --------------------------------------------------
+print("Reading parameters...")
+# -- Load yaml file and related info
+params_data = ext.read_yaml_params(params_file)
+output_dir = params_data["output_dir"]
+cdm_source_fields = params_data["cdm_source_fields"]
+
+os.makedirs(data_dir / output_dir, exist_ok=True)
+
+# Table data
+cdm_source_row = [cdm_source_fields]
+
+# %%
+# -- Table generation -------------------------------------------------
+table_cdm_source = pa.Table.from_pylist(
+    cdm_source_row, schema=omop_schemas["CDM_SOURCE"]
+)
+
+# Fill extra fields
+table_cdm_source = gen.fill_omop_table(table_cdm_source, omop_schemas["CDM_SOURCE"])
+table_cdm_source = gen.reorder_omop_table(table_cdm_source, omop_schemas["CDM_SOURCE"])
+# Cast to schema
+table_cdm_source = table_cdm_source.cast(omop_schemas["CDM_SOURCE"])
+# Save
+parquet.write_table(table_cdm_source, output_dir / "CDM_SOURCE.parquet")
