72a97c5acba0a6ff79fc642fbb24155f689b0f40
Isidoro Gutierrez <isidoro.gutierrez@juntadeandalucia.es>
2025-02-06 17:03:19 +0000

    add tests for apply_transformations

--- tests/test_table_transformer.py
+++ tests/test_table_transformer.py
@@ -6,7 +6,12 @@ from bps_to_omop.table_transformer import apply_transformation
 def test_no_transformations():
     """Test function when no transformations are specified."""
     # Create a sample table
-    data = {"column1": pa.array([1, 2, 3]), "column2": pa.array(["a", "b", "c"])}
+    data = {
+        "person_id": pa.array([1, 2, 3]),
+        "start_date": pa.array([1, 2, 3]),
+        "end_date": pa.array([4, 5, 6]),
+        "type_concept": pa.array([1, 1, 1]),
+    }
     original_table = pa.Table.from_pydict(data)
 
     # Prepare params with no transformations
@@ -19,30 +24,51 @@ def test_no_transformations():
     assert result.equals(original_table)
 
 
+def test_no_file_transformations():
+    """Test function when transformations key is present but none are
+    specified for any file."""
+    # Create a sample table
+    data = {
+        "person_id": pa.array([1, 2, 3]),
+        "start_date": pa.array([1, 2, 3]),
+        "end_date": pa.array([4, 5, 6]),
+        "type_concept": pa.array([1, 1, 1]),
+    }
+    original_table = pa.Table.from_pydict(data)
+
+    # Prepare params with no transformations
+    params = {"transformations": {}}
+
+    # Apply transformation
+    result = apply_transformation(original_table, params, "test_key")
+
+    # Assert the table remains unchanged
+    assert result.equals(original_table)
+
+
 def test_single_transformation():
     """Test applying a single transformation function."""
     # Create a sample table
-    data = {"column1": pa.array([1, 2, 3]), "column2": pa.array(["a", "b", "c"])}
+    data = {
+        "person_id": pa.array([1, 2, 3]),
+        "start_date": pa.array([1, 2, 3]),
+        "end_date": pa.array([4, 5, 6]),
+        "type_concept": pa.array([1, 1, 1]),
+    }
     original_table = pa.Table.from_pydict(data)
 
-    # Define a sample transformation function
-    def double_first_column(table):
-        # Create a new column by doubling the first column
-        new_column = pa.array(table.column("column1").to_numpy() * 2)
-        return table.set_column(
-            table.column_names.index("column1"), "column1", new_column
-        )
-
     # Prepare params with transformation
-    params = {"transformations": {"test_key": [double_first_column]}}
+    params = {"transformations": {"test_key": ["remove_end_date"]}}
 
     # Apply transformation
     result = apply_transformation(original_table, params, "test_key")
 
     # Expected result
     expected_data = {
-        "column1": pa.array([2, 4, 6]),
-        "column2": pa.array(["a", "b", "c"]),
+        "person_id": pa.array([1, 2, 3]),
+        "start_date": pa.array([1, 2, 3]),
+        "end_date": pa.array([1, 2, 3]),
+        "type_concept": pa.array([1, 1, 1]),
     }
     expected_table = pa.Table.from_pydict(expected_data)
 
@@ -53,34 +79,26 @@ def test_single_transformation():
 def test_multiple_transformations():
     """Test applying multiple transformation functions."""
     # Create a sample table
-    data = {"column1": pa.array([1, 2, 3]), "column2": pa.array(["a", "b", "c"])}
+    data = {
+        "person_id": pa.array([1, 2, 3]),
+        "start_date": pa.array([1, 2, 3]),
+        "end_date": pa.array([4, 5, 6]),
+        "type_concept": pa.array([1, 1, 1]),
+    }
     original_table = pa.Table.from_pydict(data)
 
-    # Define transformation functions
-    def double_first_column(table):
-        new_column = pa.array(table.column("column1").to_numpy() * 2)
-        return table.set_column(
-            table.column_names.index("column1"), "column1", new_column
-        )
-
-    def add_constant_to_column(table):
-        new_column = pa.array(table.column("column1").to_numpy() + 10)
-        return table.set_column(
-            table.column_names.index("column1"), "column1", new_column
-        )
-
     # Prepare params with multiple transformations
-    params = {
-        "transformations": {"test_key": [double_first_column, add_constant_to_column]}
-    }
+    params = {"transformations": {"test_key": ["melt_start_end", "remove_end_date"]}}
 
     # Apply transformation
     result = apply_transformation(original_table, params, "test_key")
 
     # Expected result
     expected_data = {
-        "column1": pa.array([22, 44, 66]),
-        "column2": pa.array(["a", "b", "c"]),
+        "person_id": pa.array([1, 2, 3, 1, 2, 3]),
+        "start_date": pa.array([1, 2, 3, 4, 5, 6]),
+        "end_date": pa.array([1, 2, 3, 4, 5, 6]),
+        "type_concept": pa.array([1, 1, 1, 1, 1, 1]),
     }
     expected_table = pa.Table.from_pydict(expected_data)
 
