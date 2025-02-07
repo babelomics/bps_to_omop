2ec60bc50032a7ece80a082443d50baf00fdc55c
Isidoro Gutierrez <isidoro.gutierrez@juntadeandalucia.es>
2025-02-07 13:19:26 +0000

    finish guide and example

--- examples/genomop_provider.py
+++ examples/genomop_provider.py
@@ -11,15 +11,6 @@ import bps_to_omop.extract as ext
 import bps_to_omop.general as gen
 from bps_to_omop.omop_schemas import omop_schemas
 
-# == Parameters ===================================================================================
-print("Generating PROVIDER table:")
-print("Loading parameters...")
-load_dotenv()
-main_dir = os.environ.get("LOCAL_DATA_DIR")
-input_dir = f"{main_dir}/omop_initial/clinical/"
-output_dir = f"{main_dir}/omop_intermediate/PROVIDER/"
-os.makedirs(output_dir, exist_ok=True)
-
 # == Parameters =======================================================
 params_file = "./package/preomop/genomop_provider_params.yaml"
 
@@ -30,51 +21,59 @@ params_data = ext.read_yaml_params(params_file)
 input_dir = params_data["input_dir"]
 output_dir = params_data["output_dir"]
 input_files = params_data["input_files"]
+column_name_map = params_data["column_name_map"]
+column_values_map = params_data["column_values_map"]
 
 os.makedirs(data_dir / output_dir, exist_ok=True)
 
-
-# -- Mapping dict ---------------------------------------------------------------------------------
-# Ideally this should be put as a single concept in the OMOP vocabularies and mapped.
-# As there are only two here, we are going to do it manually.
-espe_dict = {"ginecologia": 38003902, "aparato digestivo": 38004455}
-
 # == Load file and prepare it =====================================================================
 print("Preprocessing files...")
-# -- 02_Patologias_BPS.parquet --------------------------------------------------------------------
-f = "05_Consultas_externas.parquet"
-print(f" Processing {f}: ")
-df = pd.read_parquet(f"{input_dir}{f}")
-# Retrieve unique specialties
-unique_spe = df["ESPECIALIDAD"].unique()
-# Fill a row for each value
-provider = []
-for i, spe in enumerate(unique_spe):
-    spe_norm = gen.normalize_text(spe)
-    provider_row = {
-        "provider_id": i,
-        "provider_name": gen.normalize_text(spe),
-        "provider_source_value": spe,
-        "specialty_source_value": spe,
-        "specialty_concept_id": espe_dict[spe_norm],
-    }
-    provider.append(provider_row)
+provider_id = 0
+for f in input_files:
+    print(f" Processing {f}: ")
+    df = pd.read_parquet(data_dir / input_dir / f)
+
+    # Rename columns
+    col_map = {**column_name_map[f]}
+    df = df.rename(column_name_map[f])
+
+    # Retrieve unique specialties
+    unique_spe = df["specialty_source_value"].unique()
+    # Fill a row for each value
+    provider = []
+    for i, spe in enumerate(unique_spe):
+        provider_row = {
+            "provider_id": provider_id,
+            "specialty_source_value": spe,
+            "specialty_concept_id": column_values_map[f][spe],
+        }
+        provider.append(provider_row)
+        provider_id += 1
+
+# %%
+# == Check for codes that were not mapped =============================
+test_list = ["specialty"]
+
+for col in test_list:
+    # Check for unmapped values
+    unmapped_values = gen.find_unmapped_values(
+        df, f"{col}_source_value", f"{col}_concept_id"
+    )
+    # Apply mapping if needed
+    if len(unmapped_values) > 0:
+        print(f" No concept ID found for source values: {[*unmapped_values]}")
 
 # == Final steps ==================================================================================
 print("Formatting to OMOP...")
 # Convert to table
 provider_table = pa.Table.from_pylist(provider, schema=omop_schemas["PROVIDER"])
 
-# Fill all other columns required by the OMOP schema
-provider_table = gen.fill_omop_table(
-    provider_table, omop_schemas["PROVIDER"], verbose=1
-)
-
-# Reorder columns and cast to OMOP schema
+# Fill, reorder and cast to schema
+provider_table = gen.fill_omop_table(provider_table, omop_schemas["PROVIDER"])
 provider_table = gen.reorder_omop_table(provider_table, omop_schemas["PROVIDER"])
 provider_table = provider_table.cast(omop_schemas["PROVIDER"])
 
 # == Save to parquet ==============================================================================
 print("Saving to parquet...")
-parquet.write_table(provider_table, f"{output_dir}PROVIDER.parquet")
+parquet.write_table(provider_table, data_dir / output_dir / "PROVIDER.parquet")
 print("Done.")
