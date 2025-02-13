96dd923e6161f51a8b6664ef9c5065eeb388b934
Isidoro Gutierrez <isidoro.gutierrez@juntadeandalucia.es>
2025-02-13 17:40:16 +0000

    finish test_table_fills

--- tests/test_general_format_table.py
+++ tests/test_general_format_table.py
@@ -15,26 +15,25 @@ def test_schema():
     """Create a temporary schema structure for testing."""
     return pa.schema(
         [
-            ("var_date", pa.date32(), False),
-            ("var_float", pa.float64(), False),
             ("var_int", pa.int64(), False),
+            ("var_float", pa.float64(), False),
             ("var_string", pa.string(), False),
-            ("var_timestamp", pa.timestamp(), False),
-            ("var_date_not_null", pa.date32(), True),
-            ("var_float_not_null", pa.float64(), True),
-            ("var_int_not_null", pa.int64(), True),
-            ("var_string_not_null", pa.string(), True),
-            ("var_timestamp_not_null", pa.timestamp(), True),
+            ("var_date", pa.date32(), False),
+            ("var_timestamp", pa.timestamp("us"), False),
+            ("var_int_nullable", pa.int64(), True),
+            ("var_float_nullable", pa.float64(), True),
+            ("var_string_nullable", pa.string(), True),
         ]
     )
 
 
 # == TESTS ==============================================================================
-def test_table_fills():
+def test_table_fills(test_schema):
     """Test that columns are filled"""
     input_table = pa.table(
         {
             "var_int": [1, 1, 2],
+            "var_string": ["A", "B", "C"],
             "var_date": pa.array(
                 to_datetime(["2024-01-01", "2024-02-01", "2024-03-01"]),
                 type=pa.date32(),
@@ -43,13 +42,15 @@ def test_table_fills():
                 to_datetime(["2024-01-31", "2024-02-28", "2024-03-31"]),
                 type=pa.timestamp("us"),
             ),
-            "var_string": ["A", "B", "C"],
         }
     )
+
+    output_table = fill_omop_table(input_table, test_schema)
 
     expected_table = pa.table(
         {
             "var_int": [1, 1, 2],
+            "var_string": ["A", "B", "C"],
             "var_date": pa.array(
                 to_datetime(["2024-01-01", "2024-02-01", "2024-03-01"]),
                 type=pa.date32(),
@@ -58,14 +59,11 @@ def test_table_fills():
                 to_datetime(["2024-01-31", "2024-02-28", "2024-03-31"]),
                 type=pa.timestamp("us"),
             ),
-            "var_string": ["A", "B", "C"],
-            "var_date_not_null": pa.array(["", "", ""], type=pa.date32()),
-            "var_float_not_null": pa.array([0, 0, 0], type=pa.float64()),
-            "var_int_not_null": pa.array([0, 0, 0], type=pa.int64()),
-            "var_string_not_null": pa.array(["", "", ""], type=pa.string()),
-            "var_timestamp_not_null": pa.array(["", "", ""], type=pa.timestamp("us")),
+            "var_float": pa.array([0, 0, 0], type=pa.float64()),
+            "var_int_nullable": pa.nulls(3, type=pa.int64()),
+            "var_float_nullable": pa.nulls(3, type=pa.float64()),
+            "var_string_nullable": pa.nulls(3, type=pa.string()),
         }
     )
 
-    output_table = fill_omop_table(input_table)
-    assert_frames_equal(output_table.to_pandas(), expected_table.to_pandas())
+    assert_frame_equal(output_table.to_pandas(), expected_table.to_pandas())
