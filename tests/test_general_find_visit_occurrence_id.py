9f3c70506f5eb941a735055623b6d5ed7ca30c0b
Isidoro Gutierrez-Alvarez <isidoro.gutierrez@juntadeandalucia.es>
2025-06-05 12:15:49 +0000

    add test for same day but different hour visits

--- tests/test_general_find_visit_occurrence_id.py
+++ tests/test_general_find_visit_occurrence_id.py
@@ -151,3 +151,58 @@ def test_no_valid_visits():
 
     with pytest.raises(ValueError):
         find_visit_occurence_id(events, event_columns, visits)
+
+
+def test_same_day_different_hour():
+    """Test when there is two visits the same day but not the same hour"""
+    # -- Prepare input
+    events = pd.DataFrame(
+        {
+            "event_id": [0, 1, 2],
+            "person_id": [1, 1, 1],
+            "start_date": ["2024-01-01", "2024-01-01 08:00", "2024-01-01 12:00"],
+            "end_date": ["2024-01-01", "2024-01-01 08:00", "2024-01-01 12:00"],
+            "visit_type": ["A", "C", "D"],
+        }
+    ).assign(
+        start_date=lambda x: x["start_date"].astype("datetime64[ms]"),
+        end_date=lambda x: x["end_date"].astype("datetime64[ms]"),
+    )
+    event_columns = ["person_id", "start_date", "event_id"]
+    visits = pd.DataFrame(
+        {
+            "visit_occurrence_id": [0, 1],
+            "person_id": [1, 1],
+            "visit_start_datetime": ["2024-01-01", "2024-01-01 12:00"],
+            "visit_end_datetime": ["2024-01-01", "2024-01-01 12:00"],
+        }
+    ).assign(
+        visit_start_datetime=lambda x: x["visit_start_datetime"].astype(
+            "datetime64[ms]"
+        ),
+        visit_end_datetime=lambda x: x["visit_end_datetime"].astype("datetime64[ms]"),
+    )
+
+    # -- Prepare output
+    out = pd.DataFrame(
+        {
+            "event_id": [0, 1, 2],
+            "person_id": [1, 1, 1],
+            "start_date": ["2024-01-01 00:00", "2024-01-01 08:00", "2024-01-01 12:00"],
+            "end_date": ["2024-01-01 00:00", "2024-01-01 08:00", "2024-01-01 12:00"],
+            "visit_type": ["A", "C", "D"],
+            "visit_occurrence_id": [0, np.nan, 1],
+            "visit_start_datetime": ["2024-01-01 00:00", pd.NaT, "2024-01-01 12:00"],
+            "visit_end_datetime": ["2024-01-01 00:00", pd.NaT, "2024-01-01 12:00"],
+        }
+    ).assign(
+        start_date=lambda x: x["start_date"].astype("datetime64[ms]"),
+        end_date=lambda x: x["end_date"].astype("datetime64[ms]"),
+        visit_start_datetime=lambda x: x["visit_start_datetime"].astype(
+            "datetime64[ms]"
+        ),
+        visit_end_datetime=lambda x: x["visit_end_datetime"].astype("datetime64[ms]"),
+    )
+
+    result = find_visit_occurence_id(events, event_columns, visits)
+    pd.testing.assert_frame_equal(result, out)
