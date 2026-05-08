adab3cf6f34eac1e69010dbb75083995094ea4f9
Isidoro Gutierrez-Alvarez <isigalvarez@gmail.com>
2026-05-08 11:15:28 +0000

    add extra whitespace to improve readability

--- bps_to_omop/utils/extract.py
+++ bps_to_omop/utils/extract.py
@@ -360,7 +360,7 @@ def get_date_parser_options(
 
         # -- Verbose! --
         if verbose > 0:
-            print(f" == == {f} == == ")
+            print(f"\n == == {f} == == ")
         # --------------
 
         # Read first nrows as string with readoptions
