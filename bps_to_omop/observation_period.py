4c31105ced216ac5636b2e38f0af3cbbc6819f36
Isidoro Gutierrez-Alvarez <isigalvarez@gmail.com>
2026-06-22 07:59:49 +0000

    fix bug where it will try to remove the id column that is not created yet

--- bps_to_omop/observation_period.py
+++ bps_to_omop/observation_period.py
@@ -171,16 +171,12 @@ def process_observation_period_table(
     table = group_dates(table, n_days, verbose=verbose)
 
     # Create unique id and rename columns
-    table = (
-        table.drop("observation_period_id")
-        .with_row_index("observation_period_id")
-        .rename(
-            {
-                "visit_start_date": "observation_period_start_date",
-                "visit_end_date": "observation_period_end_date",
-                "visit_type_concept_id": "period_type_concept_id",
-            }
-        )
+    table = table.with_row_index("observation_period_id").rename(
+        {
+            "visit_start_date": "observation_period_start_date",
+            "visit_end_date": "observation_period_end_date",
+            "visit_type_concept_id": "period_type_concept_id",
+        }
     )
 
     observation_period = format_to_omop.format_table(
